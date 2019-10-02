87508352b1f8469a15e9481342687fa1b2339f68
EC2 Default User <ec2-user@ip-172-31-18-255.us-east-2.compute.internal>
2019-10-02 03:31:27 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,62 @@ class MoviesController < ApplicationController
   end
 
   def index
+    @all_ratings=Movie.all_ratings
+    redirectFlag=0
+    if params[:order]
+      @orderList=params[:order]
+    else
+      @orderList=session[:order]
+      redirectFlag=1
+      #redirect_to movies_path(order: session[:order],ratings: session[:ratings])
+    end
+    if params[:order]!= session[:order]
+      session[:order]=@orderList
+    end
+    if @orderList=="title"
+      @movies=Movie.all.order(@orderList)
+      @highlight_title = "hilite"
+    elsif @orderList=="release_date"
+      @movies=Movie.all.order(@orderList)
+      @highlight_date = "hilite"
+    else
     @movies = Movie.all
+    end
+    #@ratings=params[:ratings].keys
+    #puts @ratings
+    #render plain: params[:ratings].inspect
+    if params[:ratings]
+      @ratings=params[:ratings]
+      session[:ratings]=@ratings
+      puts "parampresent"
+      puts @ratings
+      @movies=@movies.where(rating: @ratings.keys)
+    else
+      if session[:ratings]
+        @ratings=session[:ratings]
+        puts "sessionpresent"
+        puts @ratings
+        @movies=@movies.where(rating: @ratings.keys)
+        redirectFlag=1
+        #redirect_to movies_path(order: session[:order],ratings: session[:ratings])
+      else
+        @ratings=Hash[@all_ratings.collect {|rating| [rating, rating]}]
+        puts @ratings
+        @movies=@movies
+      end
+    end
+    if params[:ratings]!= session[:ratings]
+      session[:ratings]=@ratings
+    end
+    if redirectFlag==1
+      flash.keep
+      redirect_to movies_path(order: session[:order],ratings: session[:ratings])
+    end
   end
-
+  
+  
+  
+ 
   def new
     # default: render 'new' template
   end
@@ -41,5 +94,6 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
+  
+ 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        Movie.all.distinct.pluck(:rating)
+    end
 end
